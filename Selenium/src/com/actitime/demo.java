5624c258867520a3164a81dbe12ff838a6c2b930
Hp <Hp@192.168.42.1>
2023-04-12 11:33:03 +0530

    create crm

---
+++ Selenium/src/com/actitime/demo.java
@@ -0,0 +1,25 @@
+package com.actitime;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe" );
+	}
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+			
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("http://www.instagram.com");
+			String title = driver.getTitle();
+			System.out.println(title);
+			//Thread.sleep(2000);
+			//driver.close();
+			
+		}
+	}
+
+
+
+
